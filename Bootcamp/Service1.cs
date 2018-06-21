b319060b40cc9e2852beaef188a03939246d00ad
rajkiran237 <rajkiran237@gmail.com>
2018-06-22 00:27:55 +0530

    Removed post-commit comment

--- Bootcamp/Service1.cs
+++ Bootcamp/Service1.cs
@@ -36,7 +36,7 @@ namespace Bootcamp
                 lines.AddRange(
                 new[] {
                         data,
-                        "In log test for automated build. Removed post-commit file.",
+                        "In log test for automated build.",
                         DateTime.Now.ToString()
                 });
 
