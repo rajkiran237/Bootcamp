266642f241cf7ff63536f315592a29f9e398be86
rajkiran237 <rajkiran237@gmail.com>
2018-06-21 20:56:49 +0530

    First-commit

---
+++ Bootcamp/Service1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Log("OnStart");
+        }
+
+        protected override void OnStop()
+        {
+            Log("OnStop");
+        }
+
+        private List<string> Log(string data)
+        {
+            List<string> lines = new List<string>();
+            try
+            {
+                lines.AddRange(
+                new[] {
+                        data,
+                        "In log test for automated build."
+                });
+
+                File.AppendAllLines("c:\\BootcampDemo.log.txt", lines);
+            }
+            catch (Exception ex)
+            {
+                lines.AddRange(
+                    new[] {
+                        "Exception:",
+                        ex.Message,
+                        ex.StackTrace
+                    });
+            }
+
+            return lines;
+        }
+    }
+}
