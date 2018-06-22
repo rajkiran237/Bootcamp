081438ac377fc8d64f32f3dd6b17801d808512c9
rajkiran237 <rajkiran237@gmail.com>
2018-06-22 10:25:41 +0530

    Added to start service

--- Bootcamp/Service1.cs
+++ Bootcamp/Service1.cs
@@ -36,7 +36,7 @@ namespace Bootcamp
                 lines.AddRange(
                 new[] {
                         data,
-                        "In log test for automated build.",
+                        "In log test for automated build. To start service",
                         DateTime.Now.ToString()
                 });
 
